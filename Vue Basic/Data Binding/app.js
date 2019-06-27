a57f7d1057d0ee0b1f08d12dccef3ab9e786364a
Pallab Hk <horakrishnaroy@gmail.com>
2019-06-27 17:24:57 +0600

    This is Vue Binding

--- Vue Basic/Data Binding/app.js
+++ Vue Basic/Data Binding/app.js
@@ -1,6 +1,13 @@
 var app = new Vue({
     el:"#app",
     data:{
-        Name:"Hora Krishna Roy"
+        Name:"Hora Krishna Roy",
+        Ulink:"https://github.com/Horakrishna",
+        title:"This is Github Link.",
+        imageLink:"img/story3.jpg",
+        isActive:false,
+        nameTex:"Hora Krishna Roy",
+        emailTxt:"roy@gmail.com",
+        htmlContent:"<p>This is html Content.</p>"
     }
 });
