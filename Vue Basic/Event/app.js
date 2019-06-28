8eb40c1b8a6feda630fb97dce7bd352e199ddb9d
Pallab Hk <horakrishnaroy@gmail.com>
2019-06-28 22:51:55 +0600

    This is vue methods

--- Vue Basic/Event/app.js
+++ Vue Basic/Event/app.js
@@ -1,6 +1,16 @@
 var app =new Vue({
     el:"#app",
     data:{
-        name:"Hora Krishna Roy"
+        counter:0
+    },
+    methods:{
+        incement: function(inc){
+          //  this.counter=this.counter +inc;
+             this.counter +=inc;
+        },
+        decement:function(desc){
+            //this.counter =this.counter -desc;
+            this.counter -=desc;
+        }
     }
 });
